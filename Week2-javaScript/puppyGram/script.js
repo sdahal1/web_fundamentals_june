0cd81572f8dfc805004e36134c30313352a2caeb
sdahal1 <raabdahl@gmail.com>
2022-06-28 10:55:37 -0700

    belt reviewer

--- Week2-javaScript/puppyGram/script.js
+++ Week2-javaScript/puppyGram/script.js
@@ -31,3 +31,14 @@ function cookieMonster(){
     var element = document.querySelector("#cookie-box");
     element.remove();
 }
+
+
+function incrementLike(id){
+    console.log("liking!", id);
+    var element = document.querySelector(id); //this line grabs the element from the html whose id is equal to the variable id
+    // console.log("element looks like this", element);
+    let likeCount = Number(element.innerText); //element.innerText is the like count inside the span element. We will convert that to a number and store it in a variable
+    // console.log(likeCount);
+    likeCount++; //increment the like count
+    element.innerText = likeCount; //update teh element so that its innerText is the new like count
+}
